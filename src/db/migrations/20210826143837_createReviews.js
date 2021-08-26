4e5a64dceaa7cc1f9ba3c7946452d123af5155dc
Linh <lcahu91@gmail.com>
2021-08-26 18:42:03 -0400

    fixed foreign keys byt adding forgein key in addition to constains

--- src/db/migrations/20210826143837_createReviews.js
+++ src/db/migrations/20210826143837_createReviews.js
@@ -1,22 +1,23 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable("reviews", (table) => {
-      table.increments("review_id").primary();
-      table.text("context");
-      table.integer("score");
-      table
-        .foreign("critic_id")
-        .references("critic_id")
-        .inTable("critics")
-        .onDelete("cascade");
-    table   
-        .foreign("movie_id")
-        .references("movie_id")
-        .inTable("movies")
-        .onDelete("cascade");
-  })
+    table.increments("review_id").primary();
+    table.text("context");
+    table.integer("score");
+    table.integer("critic_id").unsigned().notNullable(); 
+    table
+      .foreign("critic_id")
+      .references("critic_id")
+      .inTable("critics")
+      .onDelete("CASCADE");
+    table.integer("movie_id").unsigned().notNullable();
+    table
+      .foreign("movie_id")
+      .references("movie_id")
+      .inTable("movies")
+      .onDelete("CASCADE");
+  });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable("reviews");
 };
