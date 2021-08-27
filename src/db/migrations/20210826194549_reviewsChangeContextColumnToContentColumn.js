011a299058721c751a3b3e566f19553f21c99e16
Linh <lcahu91@gmail.com>
2021-08-26 20:12:56 -0400

    migrate change to column name

---
+++ src/db/migrations/20210826194549_reviewsChangeContextColumnToContentColumn.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.table("reviews", (table) => {
+    table.renameColumn("context", "content");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.table("reviews", (table) => {
+    table.renameColumn("content", "context");
+  });
+};
