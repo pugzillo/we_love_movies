269800f7d6e2de93afdb1362012e07bbad1594a9
Linh <lcahu91@gmail.com>
2021-08-26 21:42:53 -0400

    added error and not found handler

--- src/app.js
+++ src/app.js
@@ -8,4 +8,16 @@ app.use(express.json());
 // Routers
 app.use("/movies", moviesRouter);
 
+// Not found handler
+app.use((req, res, next) => {
+    next({ status: 404, message: `Not found: ${req.originalUrl}`});
+});
+
+// Error Handler
+app.use((error, req, res, next) => {
+    console.log(error);
+    const { status = 500, message = "Something went wrong!"} = error; 
+    res.status.json({ error: message }); 
+});
+
 module.exports = app;
