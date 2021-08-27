2c5ab7f29c02eb44887aed0a02e4e6b39779efa5
Linh <lcahu91@gmail.com>
2021-08-26 21:29:12 -0400

    added movies route files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const moviesRouter = require("./movies/movies.router");
+
+app.use(express.json());
+
+// Routers
+app.use("/movies", moviesRouter);
 
 module.exports = app;
