e1d750f0fe5432d509f67121ca986ddb663030e6
Linh <lcahu91@gmail.com>
2021-09-01 17:31:01 -0400

    added method not allowed handler

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const controller = require("./movies.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list);
-router.route("/:movieId([0-9]+)").get(controller.read);
+router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/:movieId([0-9]+)").get(controller.read).all(methodNotAllowed);
 
 module.exports = router;
