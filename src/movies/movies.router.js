e1a2cf549c244aef1b743ccc6111c5915e80420f
Linh <lcahu91@gmail.com>
2021-09-02 12:15:09 -0400

    added shared routers

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const theatersRouter = require("../theaters/theaters.router");
+const reviewsRouter = require("../reviews/reviews.router");
 
+router.use("/:movieId([0-9]+)/reviews", reviewsRouter);
+router.use("/:movieId([0-9]+)/theaters", theatersRouter);
 router.route("/").get(controller.list).all(methodNotAllowed);
 router.route("/:movieId([0-9]+)").get(controller.read).all(methodNotAllowed);
 
