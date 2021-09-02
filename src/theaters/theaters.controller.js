74dd03f3e05b8be3e1efc630d26c1189a630cde1
Linh <lcahu91@gmail.com>
2021-09-02 14:16:10 -0400

    added filter for theater route nested in movies"

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,8 +1,14 @@
 const theatersService = require("./theaters.service");
 
 async function list(req, res, next) {
-  const data = await theatersService.list();
-  res.json({ data });
+  const { movieId } = req.params;
+  if (movieId) {
+    const data = await theatersService.list(movieId);
+    res.json({ data });
+  } else {
+    const data = await theatersService.list();
+    res.json({ data });
+  }
 }
 
 module.exports = {
--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -14,7 +14,14 @@ const reduceMovies = reduceProperties("theater_id", {
   });
 
 
-function list() {
+function list(movie_id) {
+  if (movie_id) {
+    return knex("theaters as t")
+    .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+    .join("movies as m", "mt.movie_id", "m.movie_id")
+    .where({ "mt.movie_id" : movie_id})
+    .select("t.*");
+  }
   return knex("theaters as t")
     .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
     .join("movies as m", "mt.movie_id", "m.movie_id")
